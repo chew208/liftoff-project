be4d76d0f9c9dc0b4aa50350220174b901e056b2
Matt Newbold <matt.newbold208@gmail.com>
2021-04-20 16:57:41 -0500

    Initial commit

---
+++ src/test/java/mnewbold/liftoffproject/LiftoffProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package mnewbold.liftoffproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiftoffProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
