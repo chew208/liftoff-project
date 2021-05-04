54b4c7348b758894b32075a4eca252c2b5ce6875
Matt Newbold <matt.newbold208@gmail.com>
2021-05-04 15:52:43 -0500

    5/4/21

--- src/main/java/mnewbold/liftoffproject/controller/AuthenticationController.java
+++ src/main/java/mnewbold/liftoffproject/controller/AuthenticationController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-
+//hi
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
