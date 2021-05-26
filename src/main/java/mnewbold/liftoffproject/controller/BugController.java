c246848f1047097e22b165d06fe3963f242a1f7b
Matt Newbold <matt.newbold208@gmail.com>
2021-05-26 17:44:42 -0500

    update 5/26

--- src/main/java/mnewbold/liftoffproject/controller/BugController.java
+++ src/main/java/mnewbold/liftoffproject/controller/BugController.java
@@ -1,13 +1,21 @@
 package mnewbold.liftoffproject.controller;
 
+import mnewbold.liftoffproject.models.data.InsectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class BugController {
 
-    public String getBugPage(){
+    @Autowired
+    private InsectRepository insectRepository;
+
+    @GetMapping("addbug")
+    public String getBugPage(Model model){
+        model.addAttribute("bugs",this.insectRepository.findAll());
         return "addbug";
     }
 }
